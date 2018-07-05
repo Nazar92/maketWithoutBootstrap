7683e39dcb2653e9b48f9fd7ea7171ad82c9ca78
Nazar <nbuchkovskyii@gmail.com>
2018-07-05 10:33:59 +0300

    Create project

---
+++ main.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    responsive: [
+      {
+        breakpoint: 700,
+        settings: {
+          // unslick: true
+          slidesToShow: 2,
+          // slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 660,
+        settings: {
+          unslick: false,
+          infinite: true,
+          speed: 500,
+          fade: true,
+          cssEase: 'linear',
+          dots: false,
+          prevArrow: '<img src="/madia/left.png">',
+          nextArrow: '<img src="/madia/right.png">'
+        }
+      }
+    ]
+  });
+});
+
+// function open () {
+//   document.getElementById("shareIt").style="display:block";
+// };
+// function close () {
+//   document.getElementById("shareIt").style="display:none";
+// };
+document.getElementById('shareIt').onclick = openShare;
+function openShare() {
+  var shareList = document.getElementById('shareList');
+  shareList.style="display:block";
+}
+document.getElementById('close').onclick = closeShare;
+function closeShare() {
+  var closeShare = document.getElementById('shareList');
+  closeShare.style="display:none";
+}
